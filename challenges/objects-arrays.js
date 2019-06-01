c6ae1ea250d0e621240770540363cac9ab663c5f
Jashele T <jashelet@Jashele.domain>
2019-05-31 18:35:11 -0700

    added universities array and sorted alphabetically

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -73,18 +73,40 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
+// /* Request 1: Create a new array called universities that contains all the universities in the graduates array.
 
-Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+
+for(let i = 0; i < graduates.length; i++) {
+    universities[i] = graduates[i]['university']
+}
+
 console.log(universities)
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
-The resulting contact information should have a space between the first name and the email information like this: 
-Name email@example.com
 
-Log the result of your new array. */
+// Once you have the new array created, sort the universities alphabetically and log the result. */
+const universities = [];
+
+for (i = 0; i < graduates.length; i++) {
+    universities[i] = graduates[i]['university']
+ }
+
+ universities.sort();
+
+ console.log(universities)
+
+
+
+// /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
+
+// The resulting contact information should have a space between the first name and the email information like this: 
+// Name email@example.com
+
+
+
+
+// Log the result of your new array. */
 const contactInfo = [];
 console.log(contactInfo);
 
